4f1265ee75384296d9f2b5c19b6e80a8dc42b27f
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-29 12:20:15 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.radouane-oubakhane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
